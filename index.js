b69ad581ae32be18f1e9226238f505b51f76b17b
Mundo, Jr <emendez9@asu.edu>
2023-09-03 13:55:29 -0700

    data.txt upload and index.js updates

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const httpServer = require("http");
 const url = require("url");
+const fs = require("fs");
 // /////////////////
-
+// Read Data from file
+const tempCourse = fs.readFileSync(`${__dirname}/data.txt`, "utf-8");
 // Create Server
 const server = httpServer.createServer(function (req, res) {
   // call back function
@@ -9,7 +11,32 @@ const server = httpServer.createServer(function (req, res) {
   const urlParameter = url.parse(req.url, true);
   console.log(urlParameter.query);
   console.log(urlParameter.pathname);
-  res.end(` We received our first request from the client`);
+
+  if (urlParameter.query.id) {
+    // if there is a query parameter name id
+    //courses page
+    if (
+      urlParameter.pathname === "/" ||
+      urlParameter.pathname.toLowerCase() === "/courses"
+    ) {
+      res.writeHead(200, {
+        // Everything ran successfully
+        "Content-type": "text/html",
+      });
+      res.end(
+        `We received our first request from the client at resource ${urlParameter.pathname.toLowerCase()} with query parameter ${
+          urlParameter.query.id
+        }
+        ${tempCourse}`
+      );
+    } else {
+      res.writeHead(404, {
+        // Server did not find what you were looking for
+        "Content-type": "text/html",
+      });
+      res.end(`resource not found`);
+    }
+  }
 });
 
 // Start Listening to requests
